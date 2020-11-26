dce1bf4629e86fafc271f1ce039b453158f92a0b
Garrett <hoflandg@sheridancollege.ca>
2020-11-26 02:26:53 -0500

    Started project and added all @angular/schematic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { TableComponent } from './table/table.component';
+import { TreeComponent } from './tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainNavComponent},
+  { path: 'nav', component: MainNavComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'table', component: TableComponent},
+  { path: 'tree', component: TreeComponent},
+  { path: 'address_form', component: AddressFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
